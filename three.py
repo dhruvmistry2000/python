33702ff4d574999fec92aa51620cd0cec59b30f6
itzdhruv <dhruvmistry2000@gmail.com>
2022-07-15 14:02:03 +0530

    new changes

---
+++ three.py
@@ -0,0 +1,14 @@
+# print("Dhruv")
+var1 = "hello world"
+var2 = 55
+var3 = 36.7
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(var2 + var3)
+var4 = "66"
+print(int(var4) + var3)
+print("Enter yur number")
+inpnum = input()
+print(inpnum)
