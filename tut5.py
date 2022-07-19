4b2c616c0c112e6658750868f730294893877b9e
itzdhruv <dhruvmistry2000@gmail.com>
2022-07-19 13:13:32 +0530

    changes

---
+++ tut5.py
@@ -0,0 +1,9 @@
+listg = ["one", "two", "three", "four", "five", 55, 66]
+print(listg)
+nums = [1, 3, 2, 6, 5]
+print(nums[4])
+# nums.sort()
+# print(nums[::-1])
+# nums.append(5)
+# nums.insert(2, 9)
+print(nums)
