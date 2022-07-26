054a342a4bbee2a749e8b6e904cfa3959f951fc5
itzdhruv <dhruvmistry2000@gmail.com>
2022-07-26 06:10:02 +0530

    26/07

--- tut8.py
+++ tut8.py
@@ -5,7 +5,23 @@ var3 = int(input())
 
 if var3 > var2:
     print("Greater")
-if var3 == var2:
+elif var3 == var2:
     print("Equal")
 else:
     print("lower")
+
+list1 = [5, 6, 99]
+
+if 5 in list1:
+    print("True")
+
+
+
+var4 = int(input())
+
+if var4 > 18:
+    print("You can drive")
+elif var4 == 18:
+    print("Cannot decide")
+else:
+    print("you cannot drive")
